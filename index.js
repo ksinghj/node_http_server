cdab8e0eb4303ff0ae8b8f75b65375e92b5e829a
Kartar Singh Jabanda <kartarjabanda@hotmail.co.uk>
2019-10-14 08:32:04 +0100

    fix typo

--- index.js
+++ index.js
@@ -89,4 +89,4 @@ const server = http.createServer((req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`SErver running on ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
